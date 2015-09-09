a360ac0746a95499b6d0bcb4aab13a87736fd782
natxo cabre <natxo.cabre@gmail.com>
2015-09-09 20:34:13 +0200

    refactored sieve' with function guards ala erlang

--- src/eratosthenes_kata/core.clj
+++ src/eratosthenes_kata/core.clj
@@ -7,11 +7,11 @@
 (defn- remove-composites-of [x xs]
   (remove #(composite-of? % x) xs))
 
-(defn sieve' [xs]
-    (if (empty? xs)
-      []
-      (let [[x & xs'] xs]
-          (cons x (sieve' (remove-composites-of x xs))))))
+(defmulti sieve' identity)
+
+(defmethod sieve' [] [_] [])
+(defmethod sieve' :default [[x & xs]]
+  (cons x (sieve' (remove-composites-of x xs))))
 
 (defn sieve [x]
   (let [xs (range 2 (inc x))]
