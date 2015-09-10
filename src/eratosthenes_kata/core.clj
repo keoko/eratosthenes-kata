096c46366c53b4e4a5e4a20a782724ea2d6e94eb
natxo cabre <natxo.cabre@gmail.com>
2015-09-10 09:11:28 +0200

    refactoring sieve' with core.match patterns

--- src/eratosthenes_kata/core.clj
+++ src/eratosthenes_kata/core.clj
@@ -1,5 +1,5 @@
-(ns eratosthenes-kata.core)
-
+(ns eratosthenes-kata.core
+  (:require [clojure.core.match :refer [match]]))
 
 (defn- composite-of? [x y]
   (== 0 (mod x y)))
@@ -7,11 +7,11 @@
 (defn- remove-composites-of [x xs]
   (remove #(composite-of? % x) xs))
 
-(defmulti sieve' identity)
-
-(defmethod sieve' [] [_] [])
-(defmethod sieve' :default [[x & xs]]
-  (cons x (sieve' (remove-composites-of x xs))))
+(defn sieve' [xs]
+  (match [(vec xs)]
+         [([] :seq)] []
+         [([x & xs'] :seq)] (cons x 
+                                  (sieve' (remove-composites-of x xs')))))
 
 (defn sieve [x]
   (let [xs (range 2 (inc x))]
